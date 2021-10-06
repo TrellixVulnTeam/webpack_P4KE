a2695a43ecbc063d4ff2cdb07e5b0c491d9eb211
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-07 01:02:01 +0600

    Upload automatic webpack for react

---
+++ react-js/src/scripts/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "../styles/styles.css";
+import "../styles/styles.scss";
+
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Mrh Rifat" />, mountNode);
