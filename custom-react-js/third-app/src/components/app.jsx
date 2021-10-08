f1c826acb5acbd37165e1bae26febc41a1b7bd45
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-08 11:41:55 +0600

    Upload latest developed react app

---
+++ custom-react-js/third-app/src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container text-center">
+                <h2>Bismillahir Rahmanir Rahim</h2>
+                <p>Ever tried, Ever failed, Try Again. Fail Again, Fail better</p>
+            </div>
+        )
+    }
+}
+
+
+export default App
